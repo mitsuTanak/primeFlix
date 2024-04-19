2e0e5bd3e6983b722e61753409b85c7ec8a152d0
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-04-19 11:47:47 -0300

    Update index.js

--- primeflix/src/pages/Home/index.js
+++ primeflix/src/pages/Home/index.js
@@ -16,21 +16,20 @@ import 'react-circular-progressbar/dist/styles.css';
 import styles from './styles.module.css';
 import api from '../../services/api';
 
-function Home() { //Define p componente funcional Home 
-
-    const[movies, setMovies] = useState([]); // Define o estado 'movies' para armazenar a lista de filmes
-    const[loading, setLoading] = useState(true); // Define o estado 'loading' para indicar se a pagina está carregando
-    const[loadingSearch, setloadingSearch] = useState(false); // Define o estado 'loadingSearch' para indicar se a busca está carregando
-    const[search, setSearch] = useState(''); // Define o estado 'search' para armazenar o termo de buscado usuario
-    const[currentPage, setCurrentPage] = useState(1); // Define o estado 'currentPage' para armazenar o número da pagina
-    const[counts, setCounts] = useState({// Define o estado 'counts' para armazenar informações spbre p tptal de páginas e resiltados.
+function Home() { //define o componente funcional Home.
+    const [movies, setMovies] = useState([]); //Define o estado 'movies' para armazenar a lista de filmes.
+    const [loading, setLoading] = useState(true); // Define o estado 'loading' para indicar se a página está carregando.
+    const[loadingSearch, setLoadingSearch] = useState(false); //Define o estado loading para indicar se a busca esta carregando
+    const[search, setSearch] = useState(''); //Define o estado "search" para armazenar o termo de busca do usuario
+    const[currentPage, setCurrentPage] = useState(1); //Define o estado 'current' para armazenar o npumero da página atual
+    const[counts, setCounts] = useState({ //Define o estado 'count' para armazenar informações sobre ototal de páginas e resultados 
         total_pages: 500,
         total_results: 10000
+
     }); 
 
-    const formaDate = (value) => {//Funçõa para formatar a data de lançamento do filme
-
-        if (value === null) return 'Não disponivel';
+    const formatDate = (value) => {//Função para formatar a data de lançamento do filme
+        if (value === null) return 'Não Disponível';
 
         let options = {
             timeZone: 'America/Sao_Paulo',
@@ -41,140 +40,159 @@ function Home() { //Define p componente funcional Home
         };
 
         const date = new Date(value);
-        returndate.toLocalDateString('pt-br', options);
-
+        return date.toLocaleDateString('pt-br', options);
     }
 
-    const handleSubmit = (e) => {//Função para lidar com o envio do formulario de pesquisa
-
+    const handleSubmit = (e) => {//Função para lidar com o envio do formulário de pesquisa.
         e.preventDefault();
 
-        setSearch(search);//Atualiza o termo de busca
-        setcurrentPage(1); // Reseta a página atual para a primeira página
+        setSearch(search); //Atualiza o termo de busca
+        setCurrentPage(1); //Reseta a página atual para a primeira página
     }
 
-    const handleChange = (e) => {// Função para lidar com a alteração do campo de busca
+    const loadMoreItems = useRef(0); //Define uma referencia para controlar o umero de vezes que o usuario carrega mais itens.
 
-        setSearch(e.target.value);// Atualiza a busca
-        setCurrentPage(1);
-
-    }
-
-    const loadMoreItems = useRef(0); //Controla o número de vezes de carregar mais itens na pagina
-
-    useEffect(() => {//Efeito de Carregamento
-        
-        async function loadMovies() {//Função asincrona para carregar os filmes
-            const params = {//Parâmetros de requisição á API
-                params:{
-                    page: currentPage
-                }
-            };
-
-            if(search !== '') {//Se houver um termo de busca, adiciona o pârametro de busca aos parametro da requisição
-                params.params.query = search;
-            }
-
-            const url = (search === '') ? 'movie/now_playing' : 'search/movie'; //Muda a URL para corresponder a busca
-
-            const response = await api.get(url, params);//Faz a requisição á API
-
-            const {results, total_pages, total_results} = response.data;//Pega a resposta da API
-
-            setMovies((previous) =>//Atualiza a lista de filmes no estado, adicionado novos filmes se a página atual for maior que 1
-                currentPage === 1 ? results : [...preavious, ...results]
-            );
-
-            setCounts({//Atualiza as info sobre o total de paginas e resultados
-                total_pages: total_pages,
-                total_results: total_results
-            });
-
-            setloadingSearch(false);//Indica que parou de buscar
-            setLoading(false); //Indica que a pagina parou de carregar
-
+    useEffect(() => { //Efeito para carregar os filmes quando o componente é montado ou quando o termo de busca ou a página atual são alterados.
+    async function loadMovies() { //Dunção assíncrona para craregar os filmes
+    const params = { //Parâmetros da requisição à API
+        params: {
+            page: currentPage
         }
+    };
 
-        loadMovies();//Chama a função para carregar os filmes
-    }, [search, currentPage]);//Reexecuta sempre que os termos atulaizarem
+    if (search !== '') { //Define a URL da API com base no termo de busca
+        params.params.query = search;
+    }
 
+    const url = (search === '') ? 'movie/now_playing' : 'search/movie'; //Define  url Dda API com base no termo de busca
 
-    const handleLoadMore = () => {//Função para carregar mais filmes quando solicitado
-        if (loadingMoreItems.current < 2) { // Permite que reecarege só 2 vezes
-            setCurrentPage((page) => page + 1);//atualiza
-            setloadingSearch(true);//Indica que está buscando
-            loadMoreItems.current++;//Adiciona o carregamento ao contador
+    const response = await api.get(url, params); //Faz a requisição à API.
+
+    const { results, total_pages, total_results } = response.data; // Extrai os resultados da responsta da API
+
+    setMovies((previous) => //Atualiza a lista de filmes no estado, adicionando novos filmes se a página atual for maior que 1
+        currentPage === 1 ? results : [...previous, ...results]
+    );
+    
+    setCounts({//Atualixa as informações sobre o total de páginas e resultados
+        total_pages: total_pages,
+        total_results: total_results
+    });
+
+    setLoadingSearch(false);//Indica que a busca não está mais carregando
+    setLoading(false); //Indica que a página não est´´a mais carregando
+    loadMoreItems.current = 0; //reseta o npumero de vezes que o usuário carrega mais itens.
+} 
+
+    loadMovies(); //chama a função para carregar os filmes
+    }, [search, currentPage]); // o Efeito é reexecutado sempre que o termo de busca ou a página atual são alterados.
+
+    const handleLoadMore = () => {//Função para carregar mais filmes quando o usuario clica no botão 'Carregar mais Filmes'.
+        if (loadMoreItems.current < 2) { //Permitir apenas duas vezes o carregamento adicional
+            setCurrentPage((page) => page + 1); //Atualiza a página atual
+            setLoadingSearch(true); //Indica que a busca está carregando
+            loadMoreItems.current++; //Incrementa o contados de carregamento adicional
         }
+     }
+     
+     if(loading) { //Se a ´página estibver carregando, exive o componente de carregamento.
+        return (
+            <Loading text='Carregando filmes...' />
+        );
     }
 
-    if (loading) {
-        return(
-            <Loading text='Carregando filmes...'/>
-        )
-    }
-
-    return(
+    return (
         <div className={styles.container}>
             <div className={styles.title}>
-                {/* Título */}
+                {/* {area de título} */}
                 <div className={styles.grid_title}>
                     <h1>Bem-Vindo(a).</h1>
-                    <p>Milhões de filmes, séries e Pessoas para Descobrir. Explore já.</p>
+                    <p>Milhões de Filmes, Séries e Pessoas para Descobrir. Explore já.</p>
                     <form onSubmit={handleSubmit}>
-                        {/* Campo de Pesquisa */}
+                        {/* Campo de entrada para pesquisa */}
                         <input 
-                            type='search'
-                            name='search'
-                            id='search'
-                            placeholder='Pesquise pelo seus filme FAVORITO...'
-                            onChange={handleChange}
-                            value={search || ''}
+                        type="search" 
+                        name="search" 
+                        id="search" 
+                        placeholder='Pesquise pelo seu filme favorito...'
+                        onChange= {handleChange}
+                        value={search || ''} 
                         />
-                        {/* Botão de Pesquisa */}
+                        {/* Botão de pesquisa */}
                         <button type='submit'>
-                            {/* Icone */}
-                            <FontAwesomeIcon icon={faSearch} size='x1'/>
+                            {/* ícone de pesquisa */}
+                            <FontAwesomeIcon icon={faSearch} size='xl' /> 
                         </button>
-
                     </form>
-
                 </div>
-
             </div>
-
-        </div>
-
-        <div className={styles.list_movies}>
-            {
-                movies.map((movie) => (
-                    <article key={movie.id}>
-                        {/* Link para a página do filme */}
+            <div className={styles.list_movies}>
+                {
+                    //Mapeia os filmes e exibe cada um
+                    movies.map((movie) => (
+                       <article key={movie.id}>
+                        {/* link para a página do filme */}
                         <Link to={`/movie/${movie.id}`}>
                             {
-                                movie.poster_past !== null ? (
-                                    <LazyLoadImage
-                                        src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`}
-                                        effect='blur'
-                                        alt={movie.title}
-                                        title={movie.title}
-                                        placeholderSrc={placeholderImage}
+                                movie.poster_path !== null ? (
+                                    <LazyLoadImage 
+                                    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                                    effect='blur'
+                                    alt={movie.title}
+                                    title={movie.title}
+                                    placeholderSrc={placeholderImage}
                                     />
                                 ) : (
-                                    //Exibe uma imagem padrão em caso de erro
+                                    //Exib uma imagem padrão casp a imagem do filme não esteja disponível
                                     <img 
-                                        src={imageNotFound}
-                                        alt={movie.title}
-                                        title={movie.title}
+                                    src={imageNotFound} 
+                                    alt={movie.title}
+                                    title={movie.title} 
                                     />
                                 )
                             }
                         </Link>
-                        {/* Exibe uma barra d eprogresso */}
-                    </article>
+                            {/* Exibe a barra de progresso com a avaliação do filme */}
+                            <div className={styles.progressbar}>
+                                <CircularProgressbar
+                                value={movie.vote_avarage *10}
+                                text={`${movie.vote_avarage}%`}
+                                />
+                            </div>
+                            <Link to={`/movie/${movie.id}`}>
+                                {/* Exibe o título do filme com limita de caracteres  */}
+                                <strong>
+                                    {movie.title.substring(0, 18)} {movie.title.length >18 && '...'}
+                                </strong>
+                            </Link>
+                            {/* Exibe a data de lançamento do filme formatada */}
+                            <p>{formatDate(movie.release_date || null)}</p>
+                       </article>
+                    ))
+                }
 
-                ))
-                
-            }
+                {
+                    //Exibe uma mensgem de carregamento durante a busca
+                    loadingSearch && (
+                        <div className={styles.search_movies}>
+                            <img src={loadingImage} alt='Carregando filmes...' width={25} height={25}/>
+                            <h3>Carregando filmes...</h3>
+                        </div>
+                    )
+                }
+            </div>
+
+            {/* Botão para carregar mais filmes se a lisgga não estiver vazia */}
+            {movies.length > 0 && (
+                <div className={styles.search_movies}>
+                    <h2 className={styles.search_h2}>
+                        Nenhum filme foi encontrado, tente novamente...
+                    </h2>
+                </div>
+            )
+        }
         </div>
-    )
+    );
 }
+
+export default memo(Home);
